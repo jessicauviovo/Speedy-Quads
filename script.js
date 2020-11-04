19ccf9cd4a3069c7bc10d61d6cab4e7314076cf0
yayjessica <65977730+yayjessica@users.noreply.github.com>
2020-11-04 06:09:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ function changeBackground(color)
 {
 	let blue = '#0392BF';
 	let pink = '#ED48BA';
-	let grey = '#E4E4E4';
+	let textcolor = '#E4E4E4';
+	let yellow = '#FBEC06';
+	let grey = '#B3D4E0';
 
 	switch(color)
 	{
 		case blue: case pink: //blue and pink
 		document.getElementById("right-col1").style.background = color;
-		document.getElementsByTagName("ARTICLE")[0].style.color = grey;
+		document.getElementsByTagName("ARTICLE")[0].style.color = textcolor;
 		document.getElementById('shoe').src='images/skate.png';
 		document.getElementById('cart').src='images/cart.png';
 		break;  
 
-		case 'yellow': case 'white':
+		case 'yellow': case 'grey':
 		document.getElementById("right-col1").style.background = color;
 		document.getElementsByTagName("ARTICLE")[0].style.color = blue;
 		document.getElementById('shoe').src='images/skate2.png';
