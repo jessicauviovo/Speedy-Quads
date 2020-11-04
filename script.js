4c0f93365fef3898e6dfcd26bd789c6aca213bd4
yayjessica <65977730+yayjessica@users.noreply.github.com>
2020-11-04 05:53:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function changeBackground(color)
+{
+	let blue = '#0392BF';
+	let pink = '#ED48BA';
+	let grey = '#E4E4E4';
+
+	switch(color)
+	{
+		case blue: case pink: //blue and pink
+		document.getElementById("right-col1").style.background = color;
+		document.getElementsByTagName("ARTICLE")[0].style.color = grey;
+		document.getElementById('shoe').src='images/skate.png';
+		break;  
+
+		case 'yellow': case 'white':
+		document.getElementById("right-col1").style.background = color;
+		document.getElementsByTagName("ARTICLE")[0].style.color = blue;
+		document.getElementById('shoe').src='images/skate2.png';
+		document.getElementById('cart').src='images/cart1.png';
+		break;
+	}
+}
